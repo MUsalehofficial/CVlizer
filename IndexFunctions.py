5f18afbb35a78cb1edcf2cac34683b2b7902c4db
mariammamdouhh <47292075+mariammamdouhh@users.noreply.github.com>
2019-02-11 02:31:39 +0200

    Refactor allowed_file - Add array_to_lowercase() decode_unicode_chars()

--- IndexFunctions.py
+++ IndexFunctions.py
@@ -1,6 +1,15 @@
-ALLOWED_EXTENSIONS = {'pdf', 'doc', 'docx'}
+import unicodedata
 
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return filename.lower().endswith(('.pdf', '.doc', '.docx'))
+
+
+def array_to_lowercase(array):
+    return list(map(lambda item: item.lower(), array))
+
+
+def decode_unicode_chars(str):
+    return unicodedata.normalize('NFD', str).encode('ascii', 'ignore')
+
+
